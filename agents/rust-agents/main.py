620ea1aa2bc4b5f7e744155564bf939973e56dd2
quax <quaxxd.enterteiment@gmail.com>
2026-04-24 10:42:54 +0300

    added server-call function

--- agents/rust-agents/main.py
+++ agents/rust-agents/main.py
@@ -1,21 +1,25 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from datetime import datetime
+from datetime import datetime, time
+import time
 import psutil
 import uvicorn
 import maturin
+import httpx
 
 
 
 app = FastAPI()
+destination_url = 'http://127.0.0.1:8080'
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:8000/metrics"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["http://localhost:8000/metrics"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 
 
@@ -42,5 +46,36 @@ def get_metric():
 
     }
 
+
+def agent_run():
+    print("Starting agent")
+
+    while True:
+        payload = get_metric()
+
+        try:
+            with httpx.Client() as client:
+                response = client.post(destination_url, json=payload, timeout=1)
+
+                if response.status_code != 200:
+                    print('agent succeed')
+
+                else:
+                    print(f'agent failed: {response.status_code}')
+
+        except (httpx.ConnectError, httpx.ConnectTimeout):
+            print('server unavailable')
+
+        time.sleep(1)
+
+
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    try:
+        agent_run()
+    except KeyboardInterrupt:
+        print('stopped by user')
+        exit(0)
+
+
+# if __name__ == '__main__':
+#     uvicorn.run(app, host='0.0.0.0', port=8000)

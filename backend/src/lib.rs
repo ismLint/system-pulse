e7b54e0cf8386e63d206550f5559029b7c60498f
ismLint <kolyapermyacov2000@gmail.com>
2026-04-25 11:00:15 +0300

    Added models

---
+++ backend/src/lib.rs
@@ -0,0 +1,4 @@
+pub mod api;
+pub mod engine;
+pub mod models;
+pub mod network;
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+mod api;
+mod engine;
+mod models;
+mod network;
+
+use axum::{routing::post, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/metrics", post(api::packet::receive_metrics));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("Project started at http://{}", addr);
+    let lisner = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::server(listener, app).await.unwrap();
 }

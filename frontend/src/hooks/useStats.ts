fddc29d255179d694e08d7da3781e64f27d354d6
drafff <nastya.nickishkina@yandex.ru>
2026-05-02 22:33:40 +0300

    изменение useStats для графиков

--- frontend/src/hooks/useStats.ts
+++ frontend/src/hooks/useStats.ts
@@ -1,21 +1,55 @@
-import{useState,useEffect} from "react";
+import {useState,useEffect,useRef} from "react";
 
-type ServerInfo={
-  server_id: number;
-  server_name: string;
-  status: string;
-};
+export type Point ={ time:string;cpu:number;ram:number};
+export type Server ={id:number;name:string;status:string;points:Point[]};
 
-export function useStats(){
-  const[server, setServer]=useState<ServerInfo | null>(null);
-  const[loading,setLoading]=useState(true);
+export function useStats(url:string){
+  const[servers,setServers]=useState<Server[]>([]);
+  const[connected,setConnected]=useState(false);
+
+  const cache = useRef<Record<number, Server>>({});           //хранение истории
 
   useEffect(() => {
-    setTimeout(() => {
-      setServer({server_id: 0,server_name:"My server",status:"online"});
-      setLoading(false);
-    },300);
-  },[]);
+    const ws = new WebSocket(url);                            //соединение с сервером
 
-  return{server,loading};
+    ws.onopen = () => setConnected(true);                     //успешно установлено
+
+    ws.onclose = () => setConnected(false);                   //закрылось соединение
+
+    ws.onmessage = (e) => {                                   //соо от сервера
+      const list = JSON.parse(e.data);
+      
+
+      for (const m of list) {
+        const old = cache.current[m.server_id];
+
+        const newPoint = {                                    //новая точка для графика
+          time: new Date(m.time).toLocaleTimeString(),        
+          cpu: m.cpu,
+          ram: m.ram,
+        };
+
+        const points=old?[...old.points,newPoint] : [newPoint];
+
+        cache.current[m.server_id]={                        //хранение последних 30 точек
+          id: m.server_id,
+          name: m.server_name,
+          status: m.status,
+          points: points.slice(-30),
+        };
+      }
+
+      setServers(Object.values(cache.current));                //обновление списка серверов
+    };
+
+    return () => ws.close();                                   //закрыть соединение
+  }, [url]);
+
+  return { servers, connected };
 }
+
+
+
+//toLocaleTimeString — метод экземпляра объекта Date в JavaScript,
+//который возвращает строку, содержащую зависимое от языка представление
+//времени этой даты в локальном часовом поясе
